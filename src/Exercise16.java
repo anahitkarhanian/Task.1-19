c1cabc0c6c15588623d1d81aa9dd1e7b5bb465a0
anahitkarhanian <anahit.karhanyan777@gmail.com>
2021-04-27 15:48:00 +0400

    Task.1-19

---
+++ src/Exercise16.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+// Given an integer 3-digit number,
+// determine and print the second digit. Input number from console.
+public class Exercise16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int secondDigit = (number / 10) % 10;
+        System.out.println(secondDigit);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//Create Main class, define  main method(psvm+tab)
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
